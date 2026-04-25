df6c7a2cdd9a06c51f00652104b0baca0a0ef7db
taniashahida-dev <taniashahida993@gmail.com>
2026-04-25 10:22:33 +0600

    Added mount handler

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -5,8 +5,11 @@ import { MongoClient } from "mongodb";
  const client = new MongoClient('process.env.MONGO_DB_PASS')
  const db = client.db('better_auth')
 
-const auth = betterAuth({
-   database : mongodbAdapter(db,{client})
+export const auth = betterAuth({
+   database : mongodbAdapter(db,{client}),
+   emailAndPassword :{
+    enabled:true
+   }
 })
 
 export default auth;
