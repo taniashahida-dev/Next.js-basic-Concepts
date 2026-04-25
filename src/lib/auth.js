d48482fa00576d77a078fb63fc127d9e6dbec75f
taniashahida-dev <taniashahida993@gmail.com>
2026-04-25 15:35:52 +0600

    Added functionality in signu page

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,7 +2,7 @@ import { mongodbAdapter } from "@better-auth/mongo-adapter";
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 
- const client = new MongoClient('process.env.MONGO_DB_PASS')
+ const client = new MongoClient(process.env.MONGO_DB)
  const db = client.db('better_auth')
 
 export const auth = betterAuth({
